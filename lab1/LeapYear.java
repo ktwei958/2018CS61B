8ed4341de61fc9b275e2f846086f92e0ac0dc3d7
ktwei <ktwei958@163.com>
2024-12-17 19:50:16 +0800

    change for cerrect

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -16,7 +16,7 @@ public class LeapYear {
 	
 	
    public static boolean isLeapYear(int year){
-  	if(year % 400 == 0 && year % 4 == 0 && year % 100 != 0)return true;
+  	if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))return true;
 	return false;
 
   }
